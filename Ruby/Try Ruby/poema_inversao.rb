4b146789208a30003ffe0871412bfe128291bbfe
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-05 16:55:43 -0300

    poema reverse #1

---
+++ Ruby/Try Ruby/poema_inversao.rb
@@ -0,0 +1,7 @@
+poema = "Minha torrada voou da minha mão\n" +
+  "E minha torrada se foi para a lua.\n" +
+  "Mas quando eu vejo isso na televisão,\n" +
+  "Colocando nossa bandeira no cometa Halley,\n" +
+  "Mais ainda eu quero come-la."
+
+puts poema.lines.reverse
