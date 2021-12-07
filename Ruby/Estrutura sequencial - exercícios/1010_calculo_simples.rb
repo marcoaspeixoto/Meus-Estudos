03f59a6191c2f0e0f17088779a19e21b0f6d34ee
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-06 21:51:42 -0300

    exercicio 1010 correções #3

--- Ruby/Estrutura sequencial - exercícios/1010_calculo_simples.rb
+++ Ruby/Estrutura sequencial - exercícios/1010_calculo_simples.rb
@@ -1,15 +1,15 @@
-codigo_peca_1 = gets.to_i
-quantidade_peca_1 = gets.to_i
-valor_peca_1 = gets.to_f
-codigo_peca_2 = gets.to_i
-quantidade_peca_2 = gets.to_i
-valor_peca_2 = gets.to_f
+peca1 = gets.split()
+
+quantidade_peca_1 = peca1[1].to_i
+valor_peca_1 = peca1[2].to_f
+
+peca2 = gets.split()
+
+quantidade_peca_2 = peca2[1].to_i
+valor_peca_2 = peca2[2].to_f
 
 valor_total_peca_1 = quantidade_peca_1 * valor_peca_1
 valor_total_peca_2 = quantidade_peca_2 * valor_peca_2
 valor_a_pagar = valor_total_peca_1 + valor_total_peca_2
 
 puts "VALOR A PAGAR: R$ %.2f" %valor_a_pagar
-
-
-
