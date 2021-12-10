cc7d781ae4c35e319737635945c3ad93313f1559
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-10 06:31:10 -0300

    exercício 1038 #4

--- Ruby/Estrutura de seleção - exercícios/1038_lanche.rb
+++ Ruby/Estrutura de seleção - exercícios/1038_lanche.rb
@@ -3,19 +3,20 @@ entrada = gets.split
 codigo = entrada[0].to_f
 quantidade = entrada[1].to_f
 
-if codigo == 1
+case codigo
+when 1
   total = 4.00 * quantidade
-  puts "Total: R$ %.2f" %total
-elsif codigo == 2
+  puts 'Total: R$ %.2f' % total
+when 2
   total = 4.50 * quantidade
-  puts "Total: R$ %.2f" %total
-elsif codigo == 3
+  puts 'Total: R$ %.2f' % total
+when 3
   total = 5.00 * quantidade
-  puts "Total: R$ %.2f" %total
-elsif codigo == 4
+  puts 'Total: R$ %.2f' % total
+when 4
   total = 2.00 * quantidade
-  puts "Total: R$ %.2f" %total
-elsif codigo == 5
+  puts 'Total: R$ %.2f' % total
+when 5
   total = 1.50 * quantidade
-  puts "Total: R$ %.2f" %total
+  puts 'Total: R$ %.2f' % total
 end
