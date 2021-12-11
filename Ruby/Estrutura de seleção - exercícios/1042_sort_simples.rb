db565e7af99da034f4433039f7ba637520be023c
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-10 21:43:22 -0300

    exercício 1042 correções #4

--- Ruby/Estrutura de seleção - exercícios/1042_sort_simples.rb
+++ Ruby/Estrutura de seleção - exercícios/1042_sort_simples.rb
@@ -3,34 +3,33 @@ n1 = n[0].to_i
 n2 = n[1].to_i
 n3 = n[2].to_i
 
-# extrai o menor número
-if n1 < n2 && n1 < n3
+# extrai o menor e armazena em n_1
+if n1 <= n2 && n1 <= n3
   n_1 = n1
-elsif n2 < n1 && n2 < n3
+elsif n2 <= n1 && n2 <= n3
   n_1 = n2
 else
   n_1 = n3
 end
 
-# extrai o maior número
-if n1 > n2 && n1 > n3
+# extrai o maior e armazena em n_3
+if n1 >= n2 && n1 >= n3
   n_3 = n1
-elsif n2 > n1 && n2 > n3
+elsif n2 >= n1 && n2 >= n3
   n_3 = n2
 else
   n_3 = n3
 end
 
-# extrai o número intermediário
-if n1 > n_1 && n1 < n_3
+# extrai o intermediário e armazena em n_2
+if n1 >= n2 && n1 <= n3 || n1 <= n2 && n1 >= n3
   n_2 = n1
-elsif n2 > n_1 && n2 < n_3
+elsif n2 >= n1 && n2 <= n3 || n2 <= n1 && n2 >= n3
   n_2 = n2
-else
+elsif n3 >= n1 && n3 <= n2 || n3 <= n1 && n3 >= n2
   n_2 = n3
 end
 
-# imprime na ordem e como foi digitado
 puts n_1
 puts n_2
 puts n_3
