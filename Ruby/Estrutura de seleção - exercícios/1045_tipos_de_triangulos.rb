94f56b30fa944e48851dc6835dcb844be7a30216
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-10 21:33:04 -0300

    exercício 1045 correções #4

--- Ruby/Estrutura de seleção - exercícios/1045_tipos_de_triangulos.rb
+++ Ruby/Estrutura de seleção - exercícios/1045_tipos_de_triangulos.rb
@@ -1,9 +1,37 @@
 entrada = gets.split
 
-a = entrada[0].to_f
-b = entrada[1].to_f
-c = entrada[2].to_f
+n1 = entrada[0].to_f
+n2 = entrada[1].to_f
+n3 = entrada[2].to_f
 
+# extrai o menor e armazena em c
+if n1 <= n2 && n1 <= n3
+  c = n1
+elsif n2 <= n1 && n2 <= n3
+  c = n2
+else
+  c = n3
+end
+
+# extrai o maior e armazena em a
+if n1 >= n2 && n1 >= n3
+  a = n1
+elsif n2 >= n1 && n2 >= n3
+  a = n2
+else
+  a = n3
+end
+
+# extrai o intermediário e armazena em b
+if n1 >= n2 && n1 <= n3 || n1 <= n2 && n1 >= n3
+  b = n1
+elsif n2 >= n1 && n2 <= n3 || n2 <= n1 && n2 >= n3
+  b = n2
+elsif n3 >= n1 && n3 <= n2 || n3 <= n1 && n3 >= n2
+  b = n3
+end
+
+# define os tipos de triangulos
 if (a >= b + c) || (b >= a + c) || (c >= a + b)
   puts "NAO FORMA TRIANGULO"
 elsif a ** 2 == b ** 2 + c ** 2
