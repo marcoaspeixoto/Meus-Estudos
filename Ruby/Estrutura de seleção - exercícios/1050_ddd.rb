372092e6e8b69634b595cce1cb3fb11b4cf8cb63
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-16 17:07:40 -0300

    exercício 1050 #4

---
+++ Ruby/Estrutura de seleção - exercícios/1050_ddd.rb
@@ -0,0 +1,20 @@
+ddd = gets.to_i
+
+case ddd
+when 61
+  puts "Brasilia"
+when 71
+  puts "Salvador"
+when 11
+  puts "Sao Paulo"
+when 21
+  puts "Rio de Janeiro"
+when 32
+  puts "Juiz de Fora"
+when 19
+  puts "Campinas"
+when 27
+  puts "Vitoria"
+when 31
+  puts "Belo Horizonte"
+end
