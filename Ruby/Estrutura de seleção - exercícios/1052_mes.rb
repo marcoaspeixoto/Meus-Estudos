f586718cc1f1c3fbff17fde150ec44f314ed5b81
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-16 22:35:27 -0300

    exercício 1052 #4

---
+++ Ruby/Estrutura de seleção - exercícios/1052_mes.rb
@@ -0,0 +1,29 @@
+mes = gets.to_i
+
+case mes
+when 1
+  puts "January"
+when 2
+  puts "February"
+when 3
+  puts "March"
+when 4
+  puts "April"
+when 5
+  puts "May"
+when 6
+  puts "June"
+when 7
+  puts "July"
+when 8
+  puts "August"
+when 9
+  puts "September"
+when 10
+  puts "October"
+when 11
+  puts "November"
+when 12
+  puts "December"
+end
+
