9fa65cea3d5f2dbb777df769617d56db5d4556e2
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-26 23:57:07 -0300

    exercicio 1079

---
+++ Ruby/Estruturas de repetição - exercícios beecrowd/1079_medias_ponderadas.rb
@@ -0,0 +1,16 @@
+qtd_casos = gets.to_i
+
+medias = []
+
+qtd_casos.times do
+  entrada = gets.split
+  nota1 = entrada[0].to_f
+  nota2 = entrada[1].to_f
+  nota3 = entrada[2].to_f
+  media = ((nota1 * 2) + (nota2 * 3) + (nota3 * 5)) / 10
+  medias << media
+end
+
+for i in medias
+  puts "%.1f" %i
+end
