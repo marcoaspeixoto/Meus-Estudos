949a78c856060696cde2605f9324e4084d9d7ae1
Marco Peixoto <marcoaspeixoto@gmail.com>
2021-12-31 15:48:29 -0300

    exercicio 1117 otimização dos if's #5

--- Ruby/Estruturas de repetição - exercícios beecrowd/1117_validacao_de_nota.rb
+++ Ruby/Estruturas de repetição - exercícios beecrowd/1117_validacao_de_nota.rb
@@ -2,14 +2,8 @@ notas = []
 
 loop do
   nota = gets.to_f
-  if (1..10).include?(nota)
-    notas << nota
-  else
-    puts "nota invalida"
-  end
-  if notas.length == 2
-    break
-  end
+  (1..10).include?(nota) ? (notas << nota) : (puts "nota invalida")
+  break if notas.length == 2
 end
 
 puts "media = %.2f" % ((notas[0] + notas[1]) / 2)
