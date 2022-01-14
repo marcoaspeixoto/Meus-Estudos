88647eed16e107197b8bbdf77c008d9e2bb53376
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-01-14 13:06:49 -0300

    método array ::[]

---
+++ Ruby/Array/::[].rb
@@ -0,0 +1,11 @@
+# criando um novo array
+arr1 = Array.[]( 1, 'a', true)  # => [1, "a", true]
+arr2 = Array[ 1, 'a', true ]    # => [1, "a", true]
+arr3 = [ 1, 'a', true ]         # => [1, "a", true]
+
+print arr1
+puts
+print arr2
+puts
+print arr3
+
