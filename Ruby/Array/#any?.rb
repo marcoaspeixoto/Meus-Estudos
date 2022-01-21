369ee57c6d34fa964af122e38a652ab0876fbd17
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-01-21 10:32:06 -0300

    método array #any? #6

--- Ruby/Array/#any?.rb
+++ Ruby/Array/#any?.rb
@@ -5,9 +5,25 @@ vetor = [nil, false]
 puts vetor.any? # => false
 
 puts
+puts
 
 vetor = [0, 1, 2]
 puts vetor.any? {|element| element > 1 } # => true
 
 vetor = [0, 1, 2]
 puts vetor.any? {|element| element > 2 } # => false
+
+puts
+puts
+
+vetor = ['food', 'drink']
+puts vetor.any?(/foo/) # => true
+
+vetor = ['food', 'drink']
+puts vetor.any?(/bar/) # => false
+
+vetor = [0, 3, 2]
+puts vetor.any?(3) # => true
+
+vetor = [0, 1, 2]
+puts vetor.any?(4) # => false
