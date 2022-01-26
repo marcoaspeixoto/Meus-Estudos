c0b16156eb5ef5f6b37ac3b3cedc2458f9bff618
Marco Peixoto <marcoaspeixoto@gmail.com>
2022-01-26 16:49:43 -0300

    Exercício 1179 loop infinito #6

--- Ruby/Vetores - exercícios beecrowd/1179_preenchimento_de_vetor_iv.rb
+++ Ruby/Vetores - exercícios beecrowd/1179_preenchimento_de_vetor_iv.rb
@@ -1,21 +1,40 @@
 # frozen_string_literal: true
 
+
 vetor = []
 
-10.times do |i|
+15.times do |i|
   vetor[i] = gets.to_i
 end
 
-par = []
-impar = []
+loop do
+  par = []
 
-vetor.each_index do |i|
-  if vetor[i].even?
-    par << vetor[i]
-    vetor.delete(vetor[i])
+  vetor.each_index do |i|
+    next if vetor[i].nil?
+    if vetor[i].even?
+      par << vetor[i]
+      vetor[i] = nil
+    end
+    break if par.size == 5 || vetor.none?
   end
-  break if vetor.size == 5
-end
 
-print par
-print vetor
+  (par.size).times { |i| puts "par[#{i}] = #{par[i]}" }
+
+  loop do
+    impar = []
+
+    vetor.each_index do |i|
+      next if vetor[i].nil?
+      if vetor[i].odd?
+        impar << vetor[i]
+        vetor[i] = nil
+      end
+      break if impar.size == 5 || vetor.none?
+    end
+
+    (impar.size).times { |i| puts "impar[#{i}] = #{impar[i]}" }
+  end
+
+  break if vetor.none?
+end
